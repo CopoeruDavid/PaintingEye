1dc150b1f4de785a6fa3e75d0b9bd0f46f0535de
David Fame <davidfame@Davids-MacBook-Pro.local>
2022-01-19 16:42:19 +0100

    added the files for the AR app

---
+++ ARKitImageDetection/OptionView.swift
@@ -0,0 +1,21 @@
+//
+//  OptionView.swift
+//  ARKitImageDetection
+//
+//  Created by David Fame on 15/01/2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+@available(iOS 13.0, *)
+struct OptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+@available(iOS 13.0, *)
+struct OptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionView()
+    }
+}
